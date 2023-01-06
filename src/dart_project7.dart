771e5bc574a074c26d29743383d8395ab000eeee
toofastghenio <112561170+toofastghenio@users.noreply.github.com>
2023-01-06 20:50:50 +0800

    first commit

---
+++ src/dart_project7.dart
@@ -0,0 +1,8 @@
+void main(){
+
+  int counter = 1;
+  while(counter < 5 ){
+    print("hello");
+    counter++;
+  }
+}
---
+++ src/dart_project8.dart
@@ -0,0 +1,7 @@
+void main(){
+  int counter = 1;
+  do{
+    print("hello");
+    counter++;
+  }while(counter < 0);
+}
